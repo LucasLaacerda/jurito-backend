dbb954f08c2730bed749376963e9cbb903340144
Lucas Lacerda <lucaslacerdajob@gmail.com>
2025-04-03 15:20:25 -0300

    Ajustando cors

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORS para frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "https://jurito-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
